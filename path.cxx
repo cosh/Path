c3b542d2f488f0683b1540ad9996c592174aafa4
Henning Rauch <henning@rauchentwicklung.biz>
2013-03-04 10:32:38 -0800

    wired C4

---
+++ path.cxx
@@ -0,0 +1,10 @@
+#include "path.h"
+#include "c4.h"
+#include <stdio.h>
+
+void Path::Print()
+{
+  C4 c4;
+  printf("Hello, World! Das ist die erste Ableitung!\n");
+  c4.Print();
+}
---
+++ path.h
@@ -0,0 +1,11 @@
+#ifndef _path_h
+#define _path_h
+
+
+class Path
+{
+public:
+  void Print();
+};
+
+#endif
