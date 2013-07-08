aa8eed5ae487e2c723d28e9bbf48e8daf2985239
Henning Rauch <henning@rauchentwicklung.biz>
2013-07-08 18:55:16 +0200

    minor transactionManager changes

--- include/manager/transactionManager.h
+++ include/manager/transactionManager.h
@@ -27,10 +27,7 @@
 #ifndef _transactionManager_h
 #define _transactionManager_h
 
-//#include "threadSafeElement.h"
-#include "../../Dependencies/C4/Dependencies/Zeter/include/threadSafeElement.h"
-
-class TransactionManager : public ThreadSafeElement {
+class TransactionManager{
 
 };
 
